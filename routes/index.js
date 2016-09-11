2b6262a626c71edfb5345e4553ef7bf246c0d2fc
Inho Choi <inhochoi@Inhoui-MacBook-Pro.local>
2016-09-11 21:46:23 +0900

    Chat UI 추가

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/chat', function(req, res, next) {
+  res.render('chat');
 });
 
 module.exports = router;
