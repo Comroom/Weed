5bdd81d802329d88469defb05832e0484f263dc5
donnaknew <donnaknew@gmail.com>
2016-09-12 16:54:36 +0900

    login & logout 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('../controller/passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(req.isAuthenticated()){
+    res.redirect('/chat');
+  }else{
+    res.render('index', { title: 'Express' });
+  }
 });
+
+router.post('/login',
+    passport.authenticate('local', { failureRedirect: '/login_fail', failureFlash: true }),
+    function(req, res){
+      res.redirect('/login_success');
+});
+
+router.get('/login_success', ensureAuthenticated, function(req, res){
+  res.send(req.user);
+});
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+function ensureAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }else{
+    res.redirect('/');
+  }
+}
 
 module.exports = router;
